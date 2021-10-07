19bce9ebccf7219d6ae6c9a6d198f4ca3d74e084
TracyZhu0608 <yz5698@nyu.edu>
2021-10-07 14:51:31 -0400

    Added basic control for the player: left and right,  jump and double jump. Some very basic platforms

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int speed = 4;
+    public int jumpForce = 50;
+    public LayerMask groundLayer;
+    float xSpeed = 0;
+    Rigidbody2D _rigidbody;
+    public Transform feetPos;
+    public bool isgrounded = false; 
+    int jumps = 0;
+
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
+    void FixedUpdate()
+    {
+        xSpeed = Input.GetAxis("Horizontal") * speed;
+        _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
+    }
+
     void Update()
     {
-        
+        isgrounded = Physics2D.OverlapCircle(feetPos.position, .3f, groundLayer);
+        if(isgrounded){
+            jumps = 1;
+        }
+
+        if(Input.GetButtonDown("Jump") && (jumps > 0 || isgrounded))
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+            _rigidbody.AddForce(new Vector2(0,jumpForce));
+            jumps --;
+        }
     }
 }
