574c5f0f9bc596b740a3bedf908202c806868d13
WAngieZhao <47250853+WAngieZhao@users.noreply.github.com>
2021-10-13 01:02:27 -0400

    Added Sprite for Pumpkin

--- Assets/Code/MovingObstacle.cs
+++ Assets/Code/MovingObstacle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,25 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MovingObstacle : MonoBehaviour
 {
-     public float delta = 1.5f;  // Amount to move left and right from the start point   
-     public float speed = 2.0f; 
-     private Vector2 startPos;
- 
-     void Start () {
-          startPos = transform.position;
-     }
-          
-     void Update () {
-          Vector2 v = startPos;
-          v.x += delta * Mathf.Sin (Time.time * speed);
-          transform.position = v;
-     }
+    public float delta = 1.5f;  // Amount to move left and right from the start point   
+    public float speed = 2.0f;
+    private Vector2 startPos;
 
-     private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+    void Start () {
+        startPos = transform.position;
+    }
+    void Update () {
+        Vector3 charecterScale = transform.localScale;
+        Vector2 v = startPos;
+        v.x += delta * Mathf.Sin (Time.time * speed);
+        if(Mathf.Sin (Time.time * speed)>0.9f)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            charecterScale.x = Math.Abs(charecterScale.x);
         }
+        else if (Mathf.Sin (Time.time * speed)<-0.9f)
+        {
+            charecterScale.x = -Math.Abs(charecterScale.x);
+        }
+        transform.localScale = charecterScale;
+        transform.position = v;
     }
 }
