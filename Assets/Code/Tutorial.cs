3583394a7518179f44b59781cf7a02fa39d6d783
Maria <mjm1175@nyu.edu>
2021-10-14 22:16:53 -0400

    Tutorial Finished?

--- Assets/Code/Tutorial.cs
+++ Assets/Code/Tutorial.cs
@@ -7,19 +7,57 @@ public class Tutorial : MonoBehaviour
 {
     public GameObject txt1;
     public GameObject txt2;
+    public GameObject txt3;
+    public GameObject txt4;
+    public GameObject txt5;
+    public GameObject txt6;
+    public GameObject txt7;
+    public GameObject txt8;
+    public GameObject txt9;
 
 
     void Start()
     {
         txt1.SetActive(true);
         txt2.SetActive(false);
+        txt3.SetActive(false);
+        txt4.SetActive(false);
+        txt5.SetActive(false);
+        txt6.SetActive(false);
+        txt7.SetActive(false);
+        txt8.SetActive(false);
+        txt9.SetActive(false);
     }
 
    private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Tutorial")){
             Destroy(other.gameObject);
-            txt2.SetActive(true);
-            txt1.SetActive(false);
+
+            if (txt1.activeSelf){
+                txt2.SetActive(true);
+                txt1.SetActive(false);                
+            } else if (txt2.activeSelf){
+                txt3.SetActive(true);
+                txt2.SetActive(false);
+            } else if (txt3.activeSelf){
+                txt4.SetActive(true);
+                txt3.SetActive(false);
+            } else if (txt4.activeSelf){
+                txt5.SetActive(true);
+                txt4.SetActive(false);
+            } else if (txt5.activeSelf){
+                txt6.SetActive(true);
+                txt5.SetActive(false);
+            } else if (txt6.activeSelf){
+                txt7.SetActive(true);
+                txt6.SetActive(false);
+            } else if (txt7.activeSelf){
+                txt8.SetActive(true);
+                txt7.SetActive(false);
+            } else if (txt8.activeSelf){
+                txt9.SetActive(true);
+                txt8.SetActive(false);
+            }
         }
     }
 }
