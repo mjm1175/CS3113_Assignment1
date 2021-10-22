4b98226e26668a798bb5e797d2b0dceaa5b02a36
Maria <mjm1175@nyu.edu>
2021-10-22 03:40:09 -0400

    lowered BGM, working pawprint levelgates

--- Assets/Code/NextLevel.cs
+++ Assets/Code/NextLevel.cs
@@ -7,9 +7,6 @@ public class NextLevel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")){
-            // try something with "SceneManager.GetActiveScene().name"
-            // make sure it doesn't load past levels that exist
-            // update so that when you die levelnum resets to 1
             GlobalVars.levelNum++;
             SceneManager.LoadScene("Level " + GlobalVars.levelNum);
         }        
